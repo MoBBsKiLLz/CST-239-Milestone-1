fd8283759c36995f2786dfd9676b13b386f584f6
Mikey <migg_18@live.com>
2023-05-14 11:45:21 -0700

    Update javadoc for Store

---
+++ Milestone_1/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"storefront","l":"Product"},{"p":"storefront","l":"Store"},{"p":"app","l":"StoreFront"}];updateSearchResults();
